4809fcbf292df3d855aa9ed8b1b84c483ac2a588
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2024-12-14 19:55:09 +0100

    Translations in spanish completed.

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const localization = {
         tech_alert_net_explanation: "I've developed under the .NET ecosystem using different tech: <br><br> <b>• ASP.NET</b>: The old reliable. Used it to develop and maintain older applications following the MVC pattern in the Cirsa Company. <br><br> <b>• .NET Core</b>: As a newer alternative to ASP.NET, for different applications and also the development of a newer API. <br><br> <b>• Entity Framework and LINQ</b>: I developed queries in LINQ alongside Entity framework to replace the database queries with a dynamic ORM solution. <br><br> <b>• MSTest</b>: For unit testing all the .NET projects.",
 
         tech_alert_blazor_title: "Blazor",
-        tech_alert_blazor_explanation: "My newest projects in the Cirsa Company were developer in Blazor. <br> The hottest tech just from the oven, newer than a typical .NET Core app and using a similar approach to Angular. <br> I've developed server sided rendered and client web assembly rendered apps using it. ",
+        tech_alert_blazor_explanation: "My newest projects in the Cirsa Company were developer in Blazor. <br><br> The hottest tech just from the oven, newer than a typical .NET Core app and using a similar approach to Angular. <br><br> I've developed server sided rendered and client web assembly rendered apps using it. ",
 
         tech_alert_unity_title: "Unity",
         tech_alert_unity_explanation: "I'm passionate about developing videogames in different tools, one of them being the Unity Engine, which forms part of the .NET ecosystem too.",
@@ -33,16 +33,16 @@ const localization = {
         tech_alert_kotlin_explanation: "After learning the basis of Java for Android app development I stumbled upon Kotlin, which I preferred learning it by myself for modern developments, as it's syntax is nicer without compromising being a powerful language.",
 
         tech_alert_ionic_title: "Ionic",
-        tech_alert_ionic_explanation: "I applied Ionic whilst studying in a vocational training course the Ionic framework. Later I used it in my internship with the Properly Software SL company for creating hybrid apps with native functions with Capacitor alongside Angular.",
+        tech_alert_ionic_explanation: "I applied Ionic to my final project whilst studying in a vocational training course.<br><br> Later I used it in my internship with the Properly Software SL company for creating hybrid apps with native functions with Capacitor alongside Angular.",
 
         tech_alert_angular_title: "Angular",
         tech_alert_angular_explanation: "I've experienced Angular alongside Ionic, but I'm willing to explore new ways of using it, like in a standalone way or with other frameworks.",
 
         tech_alert_firebase_title: "Firebase",
-        tech_alert_firebase_explanation: "When I discovered Firebase in my vocational training course I instantly fell in love with it. It's a non relational database with some powerful capabilities. <br> First tune I used it was with Ionic and Angular, later I used it in some Android projects with Kotlin.",
+        tech_alert_firebase_explanation: "When I discovered Firebase in my vocational training course I instantly fell in love with it. It's a non relational database with some powerful capabilities. <br><br> First tune I used it was with Ionic and Angular, later I used it in some Android projects with Kotlin.",
 
         tech_alert_plsql_title: "PL/SQL",
-        tech_alert_plsql_explanation: "When working in Cirsa, I had to translate a lot of queries from the Oracle PL/SQL to entity framework, but I also made some PL/SQL new ones. Later, I learned to manage some database aspects like tables and relationships in different environments like testing, integration, production...",
+        tech_alert_plsql_explanation: "When working in Cirsa, I had to translate a lot of queries from the Oracle PL/SQL to entity framework, but I also made some PL/SQL new ones.<br><br> Later, I learned to manage some database aspects like tables and relationships in different environments like testing, integration, production...",
 
         tech_alert_lua_title: "Lua, Luau and Roblox",
         tech_alert_lua_explanation: "As a game developer too, I discovered the metaverse of Roblox and it's programming tools. <br> Roblox uses Luau, which is Lua with extended capabilities like better types support. <br><br> I made a game in the platform, it wasn't a hit but made me learn a lot of concepts about game design too. <br><br> <a href='https://bleepingdragon.com/markdown/games/DontStepThere/' target='_blank' rel='noopener noreferrer'> Check an article I made about it here</a>",
@@ -59,9 +59,42 @@ const localization = {
         hero_button_know_more: "Saber más",
 
         //Tech Skills
-        tech_skills_title: "Skills digitales"
+        tech_skills_title: "Skills digitales",
 
         //Tech Alerts
+        tech_alert_csharp_title: "C#",
+        tech_alert_csharp_explanation: "C# es el lenguaje que utilizo en todos mis proyectos de .NET.",
+
+        tech_alert_net_title: ".NET",
+        tech_alert_net_explanation: "He desarrollado en el ecosistema .NET utilizando diferentes tecnologías: <br><br> <b>• ASP.NET</b>: La vieja confiable. Lo usé para desarrollar y mantener aplicaciones más antiguas siguiendo el patrón MVC en la empresa Cirsa. <br><br> <b>• .NET Core</b>: Como una alternativa más moderna a ASP.NET, para diferentes aplicaciones y también para el desarrollo de una API más nueva. <br><br> <b>• Entity Framework y LINQ</b>: Desarrollé consultas en LINQ junto con Entity Framework para reemplazar las consultas de la base de datos con una solución ORM dinámica. <br><br> <b>• MSTest</b>: Para pruebas unitarias en todos los proyectos de .NET.",
+
+        tech_alert_blazor_title: "Blazor",
+        tech_alert_blazor_explanation: "Mis proyectos más recientes en Cirsa fueron desarrollados en Blazor. <br><br> La tecnología más reciente, más nueva que una aplicación típica de .NET Core y con un enfoque similar a Angular. <br><br> He desarrollado aplicaciones renderizadas tanto en el servidor como en el cliente usando esta tecnología.",
+
+        tech_alert_unity_title: "Unity",
+        tech_alert_unity_explanation: "Me apasiona desarrollar videojuegos en diferentes herramientas, una de ellas es el motor Unity, que también forma parte del ecosistema .NET.",
+
+        tech_alert_android_title: "Android",
+        tech_alert_android_explanation: "En mis estudios he trabajado con el desarrollo de aplicaciones nativas de Android usando Java, aunque hoy en día prefiero Kotlin. <br> Tengo experiencia desarrollando aplicaciones con diseños XML de Android y me gustaría probar Jetpack Compose o incluso Compose Multiplatform en el futuro. <br><br> También he publicado 2 aplicaciones en el momento de escribir esto en la Play Store. <br><br> <a href='https://play.google.com/store/apps/dev?id=7644279889113215830' target='_blank' rel='noopener noreferrer'> Míralas aquí</a>",
+
+        tech_alert_kotlin_title: "Kotlin",
+        tech_alert_kotlin_explanation: "Después de aprender los fundamentos de Java para el desarrollo de aplicaciones de Android, descubrí Kotlin, que preferí aprender por mi cuenta para desarrollos modernos, ya que su sintaxis es más agradable sin dejar de ser un lenguaje poderoso.",
+
+        tech_alert_ionic_title: "Ionic",
+        //I applied Ionic whilst studying in a vocational training course the Ionic framework.
+        tech_alert_ionic_explanation: "Utilicé Ionic mientras estudiaba en un curso de formación profesional para mi trabajo final.<br><br> Más tarde lo usé en mis prácticas con la empresa Properly Software SL para crear aplicaciones híbridas con funciones nativas usando Capacitor junto con Angular.",
+
+        tech_alert_angular_title: "Angular",
+        tech_alert_angular_explanation: "He trabajado con Angular junto a Ionic, pero estoy dispuesto a explorar nuevas formas de usarlo, tanto de manera independiente como con otros frameworks.",
+
+        tech_alert_firebase_title: "Firebase",
+        "tech_alert_firebase_explanation": "Cuando descubrí Firebase en mi curso de formación profesional, me enamoré instantáneamente. Es una base de datos no relacional con capacidades muy potentes. <br><br> La primera vez que lo usé fue con Ionic y Angular, luego lo utilicé en algunos proyectos de Android con Kotlin.",
+
+        tech_alert_plsql_title: "PL/SQL",
+        tech_alert_plsql_explanation: "Cuando trabajaba en Cirsa, tuve que traducir muchas consultas de Oracle PL/SQL a Entity Framework, pero también creé algunas consultas nuevas en PL/SQL. <br><br>Más tarde, aprendí a gestionar aspectos de bases de datos como tablas y relaciones en diferentes entornos como pruebas, integración, producción...",
+
+        tech_alert_lua_title: "Lua, Luau y Roblox",
+        tech_alert_lua_explanation: "Como desarrollador de juegos, descubrí el metaverso de Roblox y sus herramientas de programación. <br> Roblox utiliza Luau, que es Lua con capacidades extendidas como un mejor soporte para tipos. <br><br> Desarrollé un juego en dicha plataforma, no fue un éxito, pero me enseñó muchos conceptos sobre diseño de juegos. <br><br> <a href='https://bleepingdragon.com/markdown/games/DontStepThere/' target='_blank' rel='noopener noreferrer'> Mira un artículo que escribí sobre el juego aquí</a>"
     }
 }
 
